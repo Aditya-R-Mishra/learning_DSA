adab329e9e4921245de9bcb52ee587fd21af0e1f
Aditya-R-Mishra <adityamishra3637@gmail.com>
2026-06-28 22:21:47 +0530

    Pattern 9 and 10 added

---
+++ Patterns/pattern9.java
@@ -0,0 +1,25 @@
+// *
+// ***
+//*****
+// ***
+//  *
+
+import java.util.*;
+
+public class pattern9{
+    public static void main (String[] args){
+        int n = 5;
+        // Upper half (pyramid)
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) System.out.print(" ");
+            for (int j = 1; j <= 2*i-1; j++) System.out.print("*");
+            System.out.println();
+        }
+        // Lower half (inverted)
+        for (int i = n-1; i >= 1; i--) {
+            for (int j = 1; j <= n-i; j++) System.out.print(" ");
+            for (int j = 1; j <= 2*i-1; j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
