186f3c38ac294ceb573ed6be9037861fe43b01bc
Aditya-R-Mishra <adityamishra3637@gmail.com>
2026-06-30 20:25:56 +0530

    new patterns added

---
+++ Patterns/pattern11.java
@@ -0,0 +1,26 @@
+//1
+//0 1
+//1 0 1
+//0 1 0 1
+
+import java.util.*;
+
+class pattern11{
+    public static void main(String[] args){
+        int n = 5;
+
+        for (int i =1; i<=n; i++){
+            int start;
+            if(i%2==0) {start = 0;}
+            else {start = 1;}
+
+            for(int j = 1; j<=i; j++){
+                System.out.print(start + " ");
+
+                start = 1- start;
+            }
+
+            System.out.println();
+        }
+    }
+}
